571eb703502e432f356cf01778daa66e10314e0d
bartschliam <bartschliam@gmail.com>
2025-03-09 15:02:05 +0100

    added waitress

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return redirect('https://marketplace-personal-portfolio-zi3v2.wstd.io/', code=301)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 5000))  # Render assigns a port
+    app.run(host='0.0.0.0', port=port)
